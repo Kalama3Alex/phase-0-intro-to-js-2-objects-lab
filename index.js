821037a1f3c98109d58259385d82800277e33dd2
Kalama3Alex <alexkalama58@gmail.com>
2022-11-23 15:53:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name:"Alex",
+    streetAddress:"1937",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    var newObj = {...employee};
+    newObj = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const clone = {...employee}
+    delete clone[key];
+    return clone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
